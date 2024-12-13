1a86b8427a4ff7ce77adfa7249fcf70952872fef
michdf <michodhani01@gmail.com>
2024-12-13 20:54:51 +0700

    fix : forgot to edit it

--- src/data/data_handler.h
+++ src/data/data_handler.h
@@ -5,23 +5,23 @@
 #include "models/book.h"
 
 // Fungsi untuk memuat buku dari file JSON
-int load_books_from_json(const char *filename, struct Book *books, int max_books);
+int load_books_from_json(struct Book *books);
 
 // Fungsi untuk menyimpan buku ke file JSON
-int save_books_to_json(const char *filename, struct Book *books, int count);
+int save_books_to_json(struct Book *books, int count);
 
 // Fungsi CRUD
 
 // Menambahkan buku baru
-int add_book(const char *filename, struct Book new_book);
+int add_book(struct Book new_book);
 
 // Mengupdate buku berdasarkan ID
-int update_book(const char *filename, int id, struct Book updated_book);
+int update_book(int id, struct Book updated_book);
 
 // Menghapus buku berdasarkan ID
-int delete_book(const char *filename, int id);
+int delete_book(int id);
 
 // Mendapatkan buku berdasarkan ID
-struct Book *get_book_by_id(const char *filename, int id);
+struct Book *get_book_by_id(int id);
 
 #endif // DATA_HANDLER_H
